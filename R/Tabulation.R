9f695a162dbade4f2a3d955ff557ae43db27304e
LoukiaSpin <spineli.loukia@mh-hannover.de>
2024-12-02 17:30:15 +0100

    Update tabulation

--- R/Tabulation.R
+++ R/Tabulation.R
@@ -44,11 +44,17 @@ data_sr[, c("no_authors", "continent", "prospero")] %>%
 # Estimator of heterogeneity parameter applied: Add 'Mantel-Haenszel as 'Not reported'
 data_meta$estimator_typ <- revalue(factor(data_meta$estimator_typ), c("Mantel-Haenszel" = "Not reported"))
 
+# Merge "Insufficient" wirh unclear in the following variables in a new 'version' (_new)
+data_meta$Meta_model_choice_new <- revalue(factor(data_meta$Meta_model_choice), c("Insufficient" = "Unclear"))
+data_meta$choice_explained_new <- revalue(factor(data_meta$`choice explained`), c("Insufficient" = "Unclear"))
+data_meta$Meta_hetero_new <- revalue(factor(data_meta$Meta_hetero), c("Insufficient" = "Unclear"))
+data_meta$Interpret_heter_new <- revalue(factor(data_meta$Interpret_heter), c("Insufficient" = "Unclear"))
+
 # Collect meta-analysis level variables
 meta_level <- 
   data_meta[, c("meta_model", "estimator_typ", "tau ci", "heteror_report", "i2 ci", 
-                "est_type", "Meta_model_choice", "choice explained", 
-                "Meta_hetero", "Interpret_heter", "Interpret_tau")]
+                "est_type", "Meta_model_choice_new", "choice_explained_new", 
+                "Meta_hetero_new", "Interpret_heter_new", "Interpret_tau")]
 
 # Create Table
 meta_level %>% 
@@ -60,21 +66,20 @@ meta_level %>%
                 heteror_report ~ "categorical",
                 `i2 ci` ~ "categorical",
                 est_type ~ "categorical",
-                Meta_model_choice ~ "categorical",
-                #`choice explained` ~ "categorical",
-                Meta_hetero ~ "categorical",
-                Interpret_heter ~ "categorical",
+                Meta_model_choice_new ~ "categorical",
+                Meta_hetero_new ~ "categorical",
+                Interpret_heter_new ~ "categorical",
                 Interpret_tau ~ "categorical"),
     digits = list(all_categorical() ~ c(0, 1))) %>%
   add_overall() %>%
   italicize_levels() 
 
 # Restrict to meta-analyses that selected the model a prior
-subset(meta_level, Meta_model_choice == "Yes")[, c(1, 8)] %>% 
+subset(meta_level, Meta_model_choice_new == "Yes")[, c(1, 8)] %>% 
   tbl_summary(
     by = meta_model,
     statistic = list(all_categorical() ~ "{n} ({p}%)"),
-    type = list( `choice explained` ~ "categorical"),
+    type = list( choice_explained_new ~ "categorical"),
     digits = list(all_categorical() ~ c(0, 1))) %>%
   add_overall() %>%
   italicize_levels() 
